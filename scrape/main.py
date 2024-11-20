76010820e5dadd605d2acaec204c5ca92069d255
Anton Gersvang Golles <tonton@Antons-MacBook-Pro.local>
2024-11-20 16:00:15 +0100

    slight restructuring

--- scrape/main.py
+++ scrape/main.py
@@ -1,13 +1,11 @@
-from parse_history import get_urls
-from extract import extract
-from delete_bad_images import broken_images
+from utils import get_urls, extract, broken_images
 
 if __name__ == '__main__':
     history_path = 'scrape/history.csv'
     get_urls(history_path)   # parse history, getting the urls
 
-    # extract('dba')  # use request to get the images and texts from the urls
-    # extract('ebay')
+    extract('dba')  # use request to get the images and texts from the urls
+    extract('ebay')
 
     broken_images(base_path = 'scrape/content', delete=True, verbose=False)
 
---
+++ scrape/utils/__init__.py
@@ -0,0 +1,5 @@
+from utils.parse_history import get_urls
+from utils.extract import extract
+from utils.delete_bad_images import broken_images
+
+__all__ = ['get_urls', 'extract', 'broken_images']
