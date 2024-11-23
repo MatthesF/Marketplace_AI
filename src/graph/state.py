9ccc22988fff51ff1df04c08c0ebf00a3ea2fba9
Matthes <matthes.fogtmann@gmail.com>
2024-11-23 16:36:31 +0100

    updated state

--- src/graph/state.py
+++ src/graph/state.py
@@ -1,4 +1,5 @@
 from typing import List, TypedDict
+from PIL import Image
 
 
 class GraphState(TypedDict):
@@ -6,13 +7,13 @@ class GraphState(TypedDict):
     Represents the state of the graph.
 
     Attributes:
-        question: The question or input for the graph.
+        images: A list of images to be included.
         image_recommendation: Suggested images for the listing.
         image_description: Generated description for the listing.
         irrelevant_images: List of indices of images marked as irrelevant.
     """
-
-    question: str
+    images: List[Image.Image]
     image_recommendation: str
     image_description: str
     irrelevant_images: List[int]
+    image_titles: List[str]
