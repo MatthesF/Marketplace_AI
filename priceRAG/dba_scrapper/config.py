2d479b41bc0132b2292ef3f8c2d85568427a704e
Matthes <matthes.fogtmann@gmail.com>
2024-11-27 19:28:46 +0100

    update: minor updates

--- priceRAG/dba_scrapper/config.py
+++ priceRAG/dba_scrapper/config.py
@@ -1,6 +1,12 @@
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
 # Proxy settings
-PROXY_USERNAME = 'sp7odyv3im'
-PROXY_PASSWORD = 'kuJtM0~7Eg1Rh8ddap'
+PROXY_USERNAME = os.getenv('PROXY_USERNAME')
+PROXY_PASSWORD = os.getenv('PROXY_PASSWORD')
 PROXY_URL = f"http://{PROXY_USERNAME}:{PROXY_PASSWORD}@gate.smartproxy.com:7000"
 
 # Main URL for the listings (can be adjusted to the website being scraped)
@@ -10,5 +16,7 @@ BASE_URL = "https://www.dba.dk/soeg/side-{}"  # The placeholder {} will be repla
 START_PAGE = 2
 END_PAGE = 101
 
-# Output file for scraped data
 OUTPUT_FILE = 'scraped_data.json'
+
+# Rate-limiting setting (seconds)
+REQUEST_DELAY = 2  # Adjust this to control rate-limiting
