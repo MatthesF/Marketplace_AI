89dc7ff3baa94ebf4a76829c265a9d21a1112f90
Matthes <matthes.fogtmann@gmail.com>
2024-12-02 12:46:26 +0100

    add: new session states

--- src/utils/session_state.py
+++ src/utils/session_state.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from src.graph.state import GraphState
 
 def initialize_session_state():
     if "uploaded_files" not in st.session_state:
@@ -9,3 +10,9 @@ def initialize_session_state():
         st.session_state.processed_images = [None] * len(st.session_state.uploaded_files)
     if "recommendation" not in st.session_state:
         st.session_state.recommendation = ""
+    if "which_run" not in st.session_state:
+        st.session_state.which_run = None
+    if "graph_state" not in st.session_state:
+        st.session_state.graph_state = GraphState()
+    if "thread" not in st.session_state:
+        st.session_state.thread = {"configurable": {"thread_id": "777"}}
